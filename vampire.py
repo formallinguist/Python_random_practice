ea467a4283f78b3c596f011902f595657b532c4f
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-07-29 18:22:44 +0530

    Add files via upload

---
+++ vampire.py
@@ -0,0 +1,18 @@
+name = input('Enter the name')
+age = int(input('Enter Age'))
+
+if name == 'Alice':
+    print('Hi alice')
+    
+elif age < 12 :
+    print('Your are not alice kidoo')
+
+elif age > 2000:
+    print('Unlike you, alice is not an undead vampire')
+
+elif age > 100:
+    print('you are not alice grannie')
+
+    
+
+    
