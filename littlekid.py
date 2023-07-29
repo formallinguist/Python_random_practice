591974ec1e4796b3f2f4401a2edd887ee5dadbf6
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-07-29 18:23:04 +0530

    Add files via upload

---
+++ littlekid.py
@@ -0,0 +1,14 @@
+name = input('Enter the name')
+age = int(input('Enter Age'))
+
+if name == 'Alice':
+    print('Hi alice')
+    
+elif age < 12 :
+    print('Your are not alice kidoo')
+else:
+    print('you are neither alice nor a little kid.')
+
+    
+
+    
